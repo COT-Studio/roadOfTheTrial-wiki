640dd79f5cac888d028db70e164fa7004af40ac3
Heaveeeen <73019803+Heaveeeen@users.noreply.github.com>
2021-05-02 12:05:58 +0800

    Add files via upload

---
+++ pages/SML.js
@@ -0,0 +1,68 @@
+var sml = {
+    parse:function(str) {
+        r = "";
+        for(i = 0;i <= str.length;i++) {
+            x = str[i];
+            switch(x) {
+                case "#"://标题<h1>
+                    var id = "";//元素id
+                    var inner = "";//元素内容
+                    i++;
+                    for (; str[i] != "&"; i++) {inner += str[i]};
+                    i += 5;
+                    for (; str[i] != "#"; i++) {id += str[i]};
+                    i++;
+                    r += '<h1 id="' + id + '">' + inner + '</h1>';
+                break;
+                case "["://图像<img>
+                    var src = "";//路径
+                    var width = "";//宽度
+                    var height = "";//高度
+                    i++;
+                    for (; str[i] != "&"; i++) {src += str[i]};
+                    i += 5;
+                    for (; str[i] != "&"; i++) {width += str[i]};
+                    i += 5;
+                    for (; str[i] != "]"; i++) {height += str[i]};
+                    i++;
+                    r += '<img src="' + src + '" width="' + width + '" height="' + height + '" /';
+                break;
+                case "-"://段落<p>
+                    var inner = "";//元素内容
+                    i++;
+                    for (; str[i] != "-"; i++) {
+                        if (str[i] === "/") {//斜体字
+                            i++;
+                            inner += "<i>";
+                            for (; str[i] != "/"; i++) {inner += str[i]};
+                            inner += "</i>";
+                        } else if (str[i] === "_") {//粗体字
+                            i++;
+                            inner += "<b>";
+                            for (; str[i] != "_"; i++) {inner += str[i]};
+                            inner += "</b>";
+                        } else if (str[i] === "{") {
+                            var href = "";//目标
+                            var ainner = "";//<a>的元素内容
+                            i++;
+                            for (; str[i] != "&"; i++) {ainner += str[i]};
+                            i += 5;
+                            for (; str[i] != "}"; i++) {href += str[i]};
+                            inner += '<a href="' + href + '">' + ainner + '</a>';
+                        } else {
+                            inner += str[i];
+                        }
+                    };
+                    i++;
+                    r += '<p>' + inner + '</p>';
+                break;
+            }//switch
+        };//for
+        return r;
+    },//parse()
+    
+    wikify:function() {
+        var e = document.getElementById("section");
+        e.innerHTML = this.parse(e.innerHTML);
+    }
+}//sml
